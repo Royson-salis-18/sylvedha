15fe35823c3d2343eae25284812cca7c9b3e1398
royson salis <roysonsalis2005@gmail.com>
2026-06-26 12:53:59 +0530

    perf: disable font preloading to reduce initial network footprint and accelerate LCP on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,13 @@ import './globals.css'
 const firaSans = Fira_Sans({ 
   variable: '--font-fira-sans', 
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'] 
+  weight: ['400', '500', '600', '700'],
+  preload: false,
 })
 const fraunces = Fraunces({
   variable: '--font-fraunces',
   subsets: ['latin'],
+  preload: false,
 })
 
 export const metadata: Metadata = {
